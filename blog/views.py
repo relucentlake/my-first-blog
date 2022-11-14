a083d1efedf9f7ee52396d910cd8e3f6dbaa163f
relucent lake <relucentlake@gmail.com>
2022-11-14 22:49:57 +0300

    14.11.22

--- blog/views.py
+++ blog/views.py
@@ -8,9 +8,11 @@ from django.shortcuts import redirect
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+    
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -23,6 +25,7 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -36,3 +39,10 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+    return render(request, 'blog/post_list.html', {"posts": posts, "num_visits": num_visits})
