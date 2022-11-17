ac03c8c3a625b3ef97f13548b46f814cb11e2421
relucent lake <relucentlake@gmail.com>
2022-11-17 13:18:32 +0300

    17.11.22

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'django-insecure-$8(fo62sh^hkhpqk*7foyy4c_22n4ih^!^*e#)498&%*a4_nc1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'relucentlake.pythonanywhere.com']
 LOGIN_REDIRECT_URL = '/'
 
 
